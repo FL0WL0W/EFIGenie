733ce029265ce9aba7ef7dd0162fe2c744555931
Daniel Johnson <fl0wl0w@cracked-ice.com>
2018-02-20 03:34:44 -0600

    Voltage Service Unit Tests

--- EngineManagement/EngineManagement/VoltageService_Analog.h
+++ EngineManagement/EngineManagement/VoltageService_Analog.h
@@ -4,8 +4,8 @@ namespace EngineManagement
 	{
 		unsigned char _adcPin;
 		float A0, A1, A2, A3;
-		unsigned int _lastReadTick;
-		float _lastVoltage;
+		unsigned int _lastReadTick = 0;
+		float _lastVoltage = 0;
 		unsigned short _dotSampleRate;
 		void LoadConfig(void *config);
 	public:
