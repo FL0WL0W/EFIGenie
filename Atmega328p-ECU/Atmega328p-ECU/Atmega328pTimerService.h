66efc853390729e65d92c7a86d1920210779bac8
Daniel Johnson <FL0WL0W@Cracked-Ice.com>
2019-02-10 03:09:27 -0600

    remove old stuff. replacing with new editor soon

--- Atmega328p-ECU/Atmega328p-ECU/Atmega328pTimerService.h
+++
@@ -1,6 +0,0 @@
-class Atmega328pTimerService : public HardwareAbstraction::ITimerService
-{
-	void ScheduleCallBack(unsigned int tick);
-	unsigned int GetTick(void);
-	unsigned int GetTicksPerSecond();
-};
