390abdb1a1c8373e43fe003d0e4e9f6637590e9d
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2022-02-09 00:13:03 -0600

    readability

--- src/CommunicationHandlers/CommunicationHandler_OBD2.cpp
+++ src/CommunicationHandlers/CommunicationHandler_OBD2.cpp
@@ -23,25 +23,26 @@ namespace EFIGenie
 
 		size_t CommunicationHandler_OBD2::Receive(void *data, size_t length)
 		{
-			uint8_t service = *reinterpret_cast<uint8_t *>(data);
-			data = reinterpret_cast<uint8_t *>(data) + 1;
+			uint8_t service = *reinterpret_cast<uint8_t *>(data); //grab service from data
+			data = reinterpret_cast<uint8_t *>(data) + 1; //ofset data
 
 			switch(service)
 			{
 				case 1:
 				{
-					uint8_t pid = *reinterpret_cast<uint8_t *>(data);
-					data = reinterpret_cast<uint8_t *>(data) + 1;
+					uint8_t pid = *reinterpret_cast<uint8_t *>(data); //grab pid from data
+					data = reinterpret_cast<uint8_t *>(data) + 1; //offset data
 					switch(pid)
 					{
 						case 5:
 						{
-      						std::map<uint32_t, Variable*>::iterator it = _systemBus->Variables.find(_variableMap->EngineCoolantTempID);
+      						std::map<uint32_t, Variable*>::iterator it = _systemBus->Variables.find(_variableMap->EngineCoolantTempID); //find the variable iterator
 							if(it != _systemBus->Variables.end())
 							{
-								uint8_t ect = (*it->second + 40).To<uint8_t>();
-								_communicationService->Send(&ect, 1);
-								return 2;
+								const Variable ectVariable = *it->second; //pull variabled out of the iterator
+								uint8_t ect = (ectVariable + 40).To<uint8_t>(); //add 40 to align with -40 to 215 of obd2 pid. then convert to uint8_t
+								_communicationService->Send(&ect, 1); //send formatted ect variable back
+								return 2; //return that we parsed 2 bytes from the data received
 							}
 						}
 					}
