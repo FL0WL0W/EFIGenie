9113b04de3a6bedb27603f0d36eef21e256d5fe7
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2022-10-30 12:23:30 -0600

    TPS calibrate opposite polarity

--- EFIGenieEditor/TPS/TPS_Linear.js
+++ EFIGenieEditor/TPS/TPS_Linear.js
@@ -4,15 +4,15 @@ import UINumberWithUnit from "../UI/UINumberWithUnit.js"
 export default class TPS_Linear extends Input_AnalogPolynomial {
     static displayName = `Linear TPS`
     static outputUnits = [ `[0.0-1.0]` ]
-    static template =   `${Input_AnalogPolynomial.template}<div><label>0% Voltage:</label><div data-element="minVoltage"></div><div data-element="calibrate"></div></div><div><label>100% Voltage:</label><div data-element="maxVoltage"></div></div>`
-    minVoltage = new UINumberWithUnit({
+    static template =   `${Input_AnalogPolynomial.template}<div style="display: flex;"><div style="display: inline-block;"><div><label>0% Voltage:</label><div data-element="voltage0"></div></div><div><label>100% Voltage:</label><div data-element="voltage100"></div></div></div><div style="display: flex; align-items: center;"><div data-element="swap"></div><div data-element="calibrate"></div></div></div>`
+    voltage0 = new UINumberWithUnit({
         value:          0.1,
         step:           0.001,
         min:            0.001,
         valueUnit:      `V`,
         displayUnit:    `V`
     })
-    maxVoltage = new UINumberWithUnit({
+    voltage100 = new UINumberWithUnit({
         value:          4.9,
         step:           0.001,
         min:            0.001,
@@ -22,6 +22,9 @@ export default class TPS_Linear extends Input_AnalogPolynomial {
     calibrate = new UIButton({
         label:          `Calibrate`,
         hidden:         true
+    })
+    swap = new UIButton({
+        label:          `⤸`
     })
     get saveValue() { 
         let saveValue = super.saveValue
@@ -35,19 +38,26 @@ export default class TPS_Linear extends Input_AnalogPolynomial {
         const thisClass = this
         this.polynomial.hidden = true
         this.updatePolynomial()
-        this.minVoltage.addEventListener(`change`, function() {
+        this.voltage0.addEventListener(`change`, function() {
             thisClass.updatePolynomial()
         })
-        this.maxVoltage.addEventListener(`change`, function() {
+        this.voltage100.addEventListener(`change`, function() {
             thisClass.updatePolynomial()
         })
         this.calibrate.addEventListener(`click`, function() {
             if(thisClass.calibrate.label == `Stop`) {
                 thisClass.calibrate.label = `Calibrate`
-                thisClass.minVoltage.value = thisClass.maxVoltage.value = thisClass.voltageLiveUpdate.value
             } else {
+                thisClass.polarity = thisClass.voltage100.value < thisClass.voltage0.value ? -1 : 1
+                thisClass.voltage0.value = thisClass.voltage100.value = thisClass.voltageLiveUpdate.value
                 thisClass.calibrate.label = `Stop`
             }
+        })
+        this.swap.addEventListener(`click`, function() {
+            thisClass.polarity = -thisClass.polarity
+            const temp = thisClass.voltage0.value
+            thisClass.voltage0.value = thisClass.voltage100.value
+            thisClass.voltage100.value = temp
         })
         this.voltageLiveUpdate.addEventListener(`change`, function() {
             const liveValue = thisClass.voltageLiveUpdate.value
@@ -55,10 +65,17 @@ export default class TPS_Linear extends Input_AnalogPolynomial {
             thisClass.calibrate.hidden = liveValue == undefined
             if(liveValue != undefined) {
                 thisClass.calibrate.hidden = false
-                if(calibrating && liveValue < thisClass.minVoltage.value)
-                    thisClass.minVoltage.value = liveValue
-                if(calibrating && liveValue > thisClass.maxVoltage.value)
-                    thisClass.maxVoltage.value = liveValue
+                if(thisClass.polarity > 0) {
+                    if(calibrating && liveValue < thisClass.voltage0.value)
+                        thisClass.voltage0.value = liveValue
+                    if(calibrating && liveValue > thisClass.voltage100.value)
+                        thisClass.voltage100.value = liveValue
+                } else {
+                    if(calibrating && liveValue < thisClass.voltage100.value)
+                        thisClass.voltage100.value = liveValue
+                    if(calibrating && liveValue > thisClass.voltage0.value)
+                        thisClass.voltage0.value = liveValue
+                }
             } else {
                 thisClass.calibrate.hidden = true
             }
@@ -67,11 +84,11 @@ export default class TPS_Linear extends Input_AnalogPolynomial {
     }
 
     updatePolynomial() {
-        const min = this.minVoltage.value
-        const max = this.maxVoltage.value
+        const v0 = this.voltage0.value
+        const v100 = this.voltage100.value
         let coeffecients = []
-        coeffecients[1] = 1/(max-min)
-        coeffecients[0] = -min*coeffecients[1]
+        coeffecients[1] = 1/(v100-v0)
+        coeffecients[0] = -v0*coeffecients[1]
         this.polynomial.minValue = 0
         this.polynomial.maxValue = 1
         this.polynomial.coeffecients = coeffecients
