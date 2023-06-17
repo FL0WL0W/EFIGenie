96466b056b5750c183bac8f70159ec3888430cb9
fl0wl0w <fl0wl0w@cracked-ice.com>
2023-06-17 14:28:24 -0500

    ubuntu development environment setup script

---
+++ envinit.sh
@@ -0,0 +1,7 @@
+git submodule init
+git submodule update --progress
+sudo apt install cmake
+sudo apt install g++
+sudo wget -qO /usr/local/bin/ninja.gz https://github.com/ninja-build/ninja/releases/latest/download/ninja-linux.zip
+sudo gunzip /usr/local/bin/ninja.gz
+sudo chmod a+x /usr/local/bin/ninja
