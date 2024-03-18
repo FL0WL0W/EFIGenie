1da51a2763ad6e563364d1441c45f6e0d90e3f7e
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2024-03-18 18:36:45 -0500

    update libs

--- src/CommunicationHandlers/CommunicationHandler_OBD2.cpp
+++ src/CommunicationHandlers/CommunicationHandler_OBD2.cpp
@@ -47,7 +47,7 @@ namespace EFIGenie
 							{
 								// Engine load will be a float value between 0 and 1
 								// Need to normalize to a byte value by multiplying by 255, then perform a type conversion.
-								uint8_t cel = it->second->To<float>() * 255;
+								uint8_t cel = static_cast<float>(*it->second) * 255;
 								sendCallBack(&cel, 1);
 								return 2;
 							}
@@ -59,7 +59,7 @@ namespace EFIGenie
 							if(it != _variableMap->end())
 							{
  								//add 40 to align with -40 to 215 of obd2 pid. then convert to uint8_t
-								uint8_t ect = it->second->To<int16_t>() + 40;
+								uint8_t ect = static_cast<int16_t>(*it->second) + 40;
 								sendCallBack(&ect, 1);
 								return 2;
 							}
@@ -74,7 +74,7 @@ namespace EFIGenie
 							if(it != _variableMap->end())
 							{
 								// Min value is -100, max is 99.2. Need to normalize so it will fit in an unsigned byte.
-								uint8_t ft = static_cast<int16_t>(it->second->To<float>() * 128) + 128;
+								uint8_t ft = static_cast<int16_t>(static_cast<float>(*it->second) * 128) + 128;
 								sendCallBack(&ft, 1);
 								return 2;
 							}
@@ -87,7 +87,7 @@ namespace EFIGenie
 							{
 								// Fuel pressure is given in Bar but needs to be returned in kPa. Additionally, the byte
 								// will be multiplied by 3 by the receiver so must divide by 3 as well.
-								uint8_t fp = it->second->To<float>() * 100 / 3;
+								uint8_t fp = static_cast<float>(*it->second) * 100 / 3;
 								sendCallBack(&fp, 1);
 								return 2;
 							}
@@ -99,7 +99,7 @@ namespace EFIGenie
 							if(it != _variableMap->end())
 							{
 								// Intake manifold pressure is given in Bar but needs to be returned in kPa.
-								uint8_t imp = it->second->To<float>() * 100;
+								uint8_t imp = static_cast<float>(*it->second) * 100;
 								sendCallBack(&imp, 1);
 								return 2;
 							}
