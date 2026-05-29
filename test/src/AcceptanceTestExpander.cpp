92c187881866f123c48b79fed1bec1d41334bcf6
FL0WL0W <dhjohnson5@gmail.com>
2026-05-28 22:18:03 -0500

    add expander acceptance test

---
+++ test/src/AcceptanceTestExpander.cpp
@@ -0,0 +1,70 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "ExpanderMain.h"
+#include "MockAnalogService.h"
+#include "MockDigitalService.h"
+#include "MockPwmService.h"
+#include "MockTimerService.h"
+#include "MockCANService.h"
+#include <fstream>
+using namespace testing;
+
+using namespace OperationArchitecture;
+using namespace EmbeddedIOServices;
+using namespace EmbeddedIOOperations;
+using namespace EFIGenie;
+
+namespace UnitTests
+{
+	class AcceptanceTestExpander : public ::testing::Test 
+	{
+		protected:
+		MockAnalogService _analogService;
+		MockDigitalService _digitalService;
+		MockPwmService _pwmService;
+		MockTimerService _timerService;
+		MockCANService _canService;
+		EmbeddedIOServiceCollection _embeddedIOServiceCollection;
+		void *_config;
+		size_t _sizeOut = 0;
+		ExpanderMain *_expanderMain;
+		callback_t _crankTriggerCallback;
+		callback_t _camTriggerCallback;
+
+		AcceptanceTestExpander()
+		{
+			// size_t uint8_align = alignof(uint8_t);
+			// size_t uint16_align = alignof(uint16_t);
+			// size_t uint32_align = alignof(uint32_t);
+			// size_t uint64_align = alignof(uint64_t);
+			// size_t int8_align = alignof(int8_t);
+			// size_t int16_align = alignof(int16_t);
+			// size_t int32_align = alignof(int32_t);
+			// size_t int64_align = alignof(int64_t);
+			// size_t bool_align = alignof(bool);
+			// size_t float_align = alignof(float);
+			// size_t double_align = alignof(double);
+
+
+			_embeddedIOServiceCollection.AnalogService = &_analogService;
+			_embeddedIOServiceCollection.DigitalService = &_digitalService;
+			_embeddedIOServiceCollection.PwmService = &_pwmService;
+			_embeddedIOServiceCollection.TimerService = &_timerService;
+			_embeddedIOServiceCollection.CANService = &_canService;
+
+			std::ifstream file("config.bin", std::ios::binary | std::ios::ate);
+			std::streamsize size = file.tellg();
+			file.seekg(0, std::ios::beg);
+
+			_config = malloc(size);
+			if (file.read(reinterpret_cast<char *>(_config), size))
+			{
+				_expanderMain = new ExpanderMain(_config, _sizeOut, &_embeddedIOServiceCollection, new GeneratorMap<Variable>());
+			}
+		}
+	};
+
+	TEST_F(AcceptanceTestExpander, ParsingConfigDidNotBreak)
+	{
+	}
+}
